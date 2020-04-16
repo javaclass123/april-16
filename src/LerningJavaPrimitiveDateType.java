8166c24e7a74a61876375eda49e35d34b4f4e193
javaclass123 <neshput@gmail.com>
2020-04-15 21:45:39 -0400

    first commit

---
+++ src/LerningJavaPrimitiveDateType.java
@@ -0,0 +1,28 @@
+
+public class LerningJavaPrimitiveDateType {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		long myHouseNumner = 27567389393L;
+			char[] myHouseNumber;
+			System.out.print( myHouseNumber);
+			
+			float bankaccountbalance = 35.25F;
+					System.out.println(bankaccountbalance);
+					
+					boolean ifImtrueGoHome=true;
+					System.out.print(i);
+			
+			
+			
+			
+			
+			
+			
+
+	}
+
+}
+///
+//
